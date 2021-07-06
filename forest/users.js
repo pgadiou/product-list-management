69eaf479f46c2d2fa16b0e5daaaacc02b0ad2ab7
Philippe Gadiou <philippe.gadiou@gmail.com>
2021-07-06 17:57:35 +0200

    added users model and listing user relationship

--- forest/users.js
+++ forest/users.js
@@ -1,31 +1,12 @@
 const { collection } = require('forest-express-sequelize');
-const { users } = require('../models');
 
 // This file allows you to add to your Forest UI:
 // - Smart actions: https://docs.forestadmin.com/documentation/reference-guide/actions/create-and-manage-smart-actions
 // - Smart fields: https://docs.forestadmin.com/documentation/reference-guide/fields/create-and-manage-smart-fields
 // - Smart relationships: https://docs.forestadmin.com/documentation/reference-guide/relationships/create-a-smart-relationship
 // - Smart segments: https://docs.forestadmin.com/documentation/reference-guide/segments/smart-segments
-collection('productListings', {
-  actions: [{
-    name: 'download csv',
-    type: 'single',
-    download: true,
-  },{
-    name: 'upload reviewed csv',
-    type: 'single',
-    fields: [{
-      field: 'file',
-      type: 'string',
-      widget: 'file picker',
-    }]
-  }],
-  fields: [{
-    field: 'database user',
-    type: 'String',
-    reference: 'users',
-    get: (record) => users.findOne({ where: { email: record.createdBy }}).then((user) => user).catch((e) => null)
-  }],
+collection('users', {
+  actions: [],
+  fields: [],
   segments: [],
-  searchFields: ['createdBy'],
 });
